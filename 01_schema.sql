9b26dcc7f696e7d5b4d659c6bd3026cddb970de9
SangHo.Lee <vkdnjpo1512@naver.com>
2026-01-09 10:15:22 +0900

    Create 01_schema.sql
    add employee table and sample data

---
+++ 01_schema.sql
@@ -0,0 +1,27 @@
+/*
+[질문]
+부서별로 가장 급여가 높은 직원의 이름(name), 부서(department), 급여(salary)를 조회하는 SQL 쿼리를 작성합니다.
+*/
+
+# [SETTING]
+SHOW TABLES;
+show DATABASES;
+USE practice;
+# DROP TABLE employee;
+CREATE TABLE employee (
+  id INT,
+  name VARCHAR(255),
+  department VARCHAR(255),
+  salary INT,
+  PRIMARY KEY (id)
+);
+INSERT INTO
+  employee (id, name, department, salary)
+VALUES (101, 'Max', 'Sales', 1300),
+	(102, 'Katty', 'Management', 150000),
+	(103, 'Richard', 'Management', 250000),
+	(104, 'Jennifer', 'Sales', 1000),
+	(105, 'George', 'Management', 100000),
+	(106, 'Laila', 'Sales', 1000),
+	(107, 'Sarah', 'employeeSales', 2000);
+SELECT * FROM employee;
